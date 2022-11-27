5fd2d0c886a8b9b065682e721ac64ad1bbe60827
Gillian Trethewey <gilliantrethewey100@gmail.com>
2022-11-27 14:00:19 -0500

    remove dups and remove falsey

---
+++ 18 Remove Dups in Arrays/removeDupsArrays.js
@@ -0,0 +1,14 @@
+/*
+Given two or more arrays, write a function that combines their 
+elements into one array without any repetition. 
+*/
+
+const removeDups = (...arrays) => {
+  let jointArray = [];
+
+  arrays.forEach((array) => {
+    jointArray = [...jointArray, ...array];
+  });
+  return [...new Set([...jointArray])];
+};
+console.log(removeDups([1, 2, 3, 3, 3], [1, 4, 5, 2])); // should return [1,2,3,4,5]

9bcc60fedc51049f3446f75194ababff9467dc45
Gillian Trethewey <gilliantrethewey100@gmail.com>
2022-11-30 18:16:32 -0500

    added loop method

--- 25 Absent Vowel/absentVowel.js
+++ 25 Absent Vowel/absentVowel.js
@@ -22,6 +22,15 @@ Examples
 //   }
 // };
 
+// const absentVowels = (x) => {
+//   let vowels = 'aeiou';
+//   for (let i = 0; i < vowels.length; i++){
+//     if (str.indexOf(vowels[i]) === -1) {
+//       return i;
+//     }
+//   }
+// }
+
 const absentVowel = (str) => {
   return [..."aeiou"].findIndex((e) => !str.includes(e));
 };
